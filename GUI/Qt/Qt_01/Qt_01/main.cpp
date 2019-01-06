f810a6bd7a5786e825dbb2d45bbca52e38fe28fc
Ovins <ovins@outlook.com>
2019-01-06 16:34:48 +0800

    try

--- GUI/Qt/Qt_01/Qt_01/main.cpp
+++ GUI/Qt/Qt_01/Qt_01/main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include<QPushButton>
+#include<QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    /*  1 创建一个QLabel
+
+        QLabel * label = new QLabel("hello,world");
+        label->show();
+    */
+
+    QPushButton *button = new QPushButton("Quit");
+    QObject::connect(button, SIGNAL(clicked()), &a, SLOT(quit()));
+    button->show();
 
     return a.exec();
 }
