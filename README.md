49de99c486322ca933b243978cee37f6e3696fec
ovins <ovins@outlook.com>
2023-06-30 11:29:48 +0800

    解决生成错误

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # notes
+
 ## 简介
 
 个人学习笔记,托管于`Github Pages`,使用`mdbook`进行构建。[点击访问](https://ovinsxu.github.io/notes/)
 
-mdbook编写指南：[点击查看](https://hellowac.github.io/mdbook-doc-zh/zh-cn/index.html)
+1. 安装rust
+2. 安装mdbook
+3. 创建项目
+4. 书写博客
+5. 上传github，设置自动构建
+
+`mdbook`编写指南：[点击查看](https://hellowac.github.io/mdbook-doc-zh/zh-cn/index.html)
 
 ## 编写方式
 
@@ -13,7 +20,4 @@ mdbook编写指南：[点击查看](https://hellowac.github.io/mdbook-doc-zh/zh-
 
 - src ： mdbook的生成源，里面存放`md`文件，会自动生成静态网页。
 - src/SUMMARY.md ： 网页侧边的文章导航，需要在这里登记。
-- stages ：草稿暂存区。
-
-
-
+- stages ：草稿暂存区。 
---
+++ generate_summary.py
@@ -0,0 +1,52 @@
+import os
+
+# 生成目录
+def generate_summary(directory_path, level=0):
+    if not os.path.isdir(directory_path):
+        return
+
+    indent = "  " * level
+    dirs = []
+    files = []
+
+    # 遍历目录，将文件和子目录分别存储到对应的列表中
+    for entry in os.scandir(directory_path):
+        if entry.is_file():
+            files.append(entry.name)
+        elif entry.is_dir():
+            dirs.append(entry.name)
+
+    # 当前目录下的文件
+    for file_name in sorted(files):
+        if file_name != "SUMMARY.md":
+            chapter_title = os.path.splitext(file_name)[0]  #去除扩展名获取章节标题
+            chapter_path = os.path.join(directory_path, file_name) 
+            relative_path = os.path.relpath(chapter_path, root_directory)
+            link = f"- [{chapter_title}]({relative_path})\n"
+            with open("./src/SUMMARY.md", "a") as summary_file:
+                summary_file.write(f"{indent}{link}")
+
+    # 递归子目录
+    for i, dir_name in enumerate(sorted(dirs)):
+        subdirectory_path = os.path.join(directory_path, dir_name)
+        with open("./src/SUMMARY.md", "a") as summary_file:
+            summary_file.write(f"{indent}- [{dir_name}]({dir_name}/README.md)\n")
+        generate_summary(subdirectory_path,level+1)
+
+# 指定包含章节的文件夹路径
+root_directory = "./src" 
+summary_path = root_directory+"/SUMMARY.md"
+
+#先把SUMMARY.md删除
+if os.path.exists(summary_path):
+    try:
+        os.remove(summary_path)
+        print("文件删除成功")
+    except OSError as e:
+        print(f"文件删除失败: {e}")
+
+# 生成新的
+with open("./src/SUMMARY.md", "w") as summary_file:
+        summary_file.write("# 目录\n\n[博客目录](SUMMARY.md)\n\n[草稿](https://github.com/OvinsXu/notes/draft)\n\n---\n\n")
+
+generate_summary(root_directory)
