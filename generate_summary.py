1abc3dc3cc510e2a4c6d009524d0e8b8e3e88e69
ovins <ovins@outlook.com>
2023-07-02 22:56:11 +0800

    修复summary不能使用外部连接的错误

--- generate_summary.py
+++ generate_summary.py
@@ -1,7 +1,16 @@
 import os
 
+# 删除目录文件
+def delete_summary(summary_path):
+    if os.path.exists(summary_path):
+        try:
+            os.remove(summary_path)
+            print("文件删除成功")
+        except OSError as e:
+            print(f"文件删除失败: {e}")
+
 # 生成目录
-def generate_summary(directory_path, level=0):
+def generate_summary(directory_path, summary_path,level=0):
     if not os.path.isdir(directory_path):
         return
 
@@ -18,35 +27,48 @@ def generate_summary(directory_path, level=0):
 
     # 当前目录下的文件
     for file_name in sorted(files):
-        if file_name != "SUMMARY.md":
+        if "SUMMARY.md" not in file_name:
             chapter_title = os.path.splitext(file_name)[0]  #去除扩展名获取章节标题
             chapter_path = os.path.join(directory_path, file_name) 
-            relative_path = os.path.relpath(chapter_path, root_directory)
-            link = f"- [{chapter_title}]({relative_path})\n"
-            with open("./src/SUMMARY.md", "a") as summary_file:
+            
+            if "draft" in chapter_path:
+                relative_path = os.path.relpath(chapter_path, draft_directory)
+                link = f"- [{chapter_title}](https://github.com/OvinsXu/notes/blob/master/draft/{relative_path})\n"
+            else:
+                relative_path = os.path.relpath(chapter_path, root_directory)
+                link = f"- [{chapter_title}]({relative_path})\n"
+            with open(summary_path, "a") as summary_file:
                 summary_file.write(f"{indent}{link}")
 
     # 递归子目录
     for i, dir_name in enumerate(sorted(dirs)):
         subdirectory_path = os.path.join(directory_path, dir_name)
-        with open("./src/SUMMARY.md", "a") as summary_file:
-            summary_file.write(f"{indent}- [{dir_name}]()\n")
-        generate_summary(subdirectory_path,level+1)
 
-# 指定包含章节的文件夹路径
-root_directory = "./src" 
+        if "draft" in subdirectory_path:
+            relative_path = os.path.relpath(subdirectory_path, draft_directory)
+            link = f"https://github.com/OvinsXu/notes/blob/master/draft/{relative_path}"
+        else:
+            link = ""
+
+        with open(summary_path, "a") as summary_file:
+            summary_file.write(f"{indent}- [{dir_name}]({link})\n")
+        generate_summary(subdirectory_path,summary_path,level+1)
+
+# 正文和草稿的目录文件
+root_directory = "./src"
 summary_path = root_directory+"/SUMMARY.md"
 
-#先把SUMMARY.md删除
-if os.path.exists(summary_path):
-    try:
-        os.remove(summary_path)
-        print("文件删除成功")
-    except OSError as e:
-        print(f"文件删除失败: {e}")
+draft_directory = "./draft"
+draft_summary_path = root_directory+"/DRAFT_SUMMARY.md"
+
+delete_summary(summary_path)
+delete_summary(draft_summary_path)
 
 # 生成新的
-with open("./src/SUMMARY.md", "w") as summary_file:
-        summary_file.write("# 目录\n\n[博客目录](SUMMARY.md)\n\n[草稿](https://github.com/OvinsXu/notes/tree/master/draft)\n\n---\n\n")
+with open(summary_path, "w") as summary_file:
+        summary_file.write("# 目录\n\n[博客目录](SUMMARY.md)\n\n[草稿目录](./DRAFT_SUMMARY.md)\n\n---\n\n")
+with open(draft_summary_path, "w") as summary_file:
+        summary_file.write("# 目录\n\n[博客目录](SUMMARY.md)\n\n[草稿目录](./DRAFT_SUMMARY.md)\n\n---\n\n")
 
-generate_summary(root_directory)
+generate_summary(root_directory,summary_path)
+generate_summary(draft_directory,draft_summary_path)
